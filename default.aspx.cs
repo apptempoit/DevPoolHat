61565b636bcaba464c9eb9b69b643513dabf075e
Apptempoit <Apptempoit@gmail.com>
2022-04-27 14:46:41 +0700

    okkk

--- default.aspx.cs
+++ default.aspx.cs
@@ -471,7 +471,7 @@ namespace DevPool
 
 
 
-
+        //แสดงกราฟ
         public void DivChart()
         {
 
@@ -548,21 +548,6 @@ namespace DevPool
                 chart += "borderWidth: 1,";
                 chart += "label: \"จำนวนคนแต่ละกลุ่ม\"}"; // Chart color
 
-                // start
-
-                /*
-                    chart += ",{ data: [";
-                //   chart += value1;
-
-                chart += "'100','200'";
-                chart += "],";
-                    chart += "backgroundColor:['rgba(155, 99, 152, 0.2)','rgba(155, 199, 132, 0.2)','rgba(215, 99, 232, 0.2)','rgba(185, 129, 132, 0.2)','rgba(125, 125, 80, 0.2)','rgba(25, 79, 32, 0.2)','rgba(255, 99, 132, 0.2)','rgba(255, 149, 64, 0.2)', 'rgba(255, 205, 86, 0.2)','rgba(75, 192, 192, 0.2)','rgba(54, 162, 235, 0.2)','rgba(153, 102, 255, 0.2)','rgba(201, 203, 207, 0.2)'],";
-                    chart += "borderColor:[ 'rgb(155, 99, 152)','rgb(155, 199, 132)','rgb(215, 99, 232)','rgb(180, 129, 132)','rgb(125, 125, 80)','rgb(25, 79, 32)','rgb(255, 99, 132)','rgb(255, 149, 64)', 'rgb(255, 205, 86)','rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)','rgb(201, 203, 207)'],";
-                    chart += "borderWidth: 1,";
-                    chart += "label: \"ค่าใช้จ่ายตามเกณฑ์(ล้านบาท)\",borderColor: \"#3e95cd\",fill: true}"; // Chart color
-                    //   chart += "]},options: { title: { display: true,text: 'Dashboard'}}"; // Chart title
-                  
-                */
                 chart += "]"; // Chart title
 
 
@@ -572,10 +557,6 @@ namespace DevPool
                 chart += "</script>";
 
                 ltChart.Text = chart;
-
-
-
-
 
 
 
